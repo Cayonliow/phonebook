6124220f3ff5e9ff23741b31f6f694b871a6ce10
Cayon <cayon.1318.96@hotmail.com>
2017-02-27 00:18:58 +0800

    26 Feb: use a smaller size of structure as the main searching structure

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -3,14 +3,13 @@
 
 #define MAX_LAST_NAME_SIZE 16
 
-/* TODO: After modifying the original version, uncomment the following
- * line to set OPT properly */
 
 #define OPT 1
+
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
-    /*1at version: Changing the size of the data structure*/
-    /*char firstName[16];
+    /*2nd version: use a smaller size of structure as the main searching structure*/
+    char firstName[16];
     char email[16];
     char phone[10];
     char cell[10];
@@ -18,9 +17,16 @@ typedef struct __PHONE_BOOK_ENTRY {
     char addr2[16];
     char city[16];
     char state[2];
-    char zip[5];*/
+    char zip[5];
     struct __PHONE_BOOK_ENTRY *pNext;
+} secondary_entry;
+
+typedef struct __LAST_NAME_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    secondary_entry *detail;
+    struct __LAST_NAME_ENTRY *pNext;
 } entry;
+
 
 entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
